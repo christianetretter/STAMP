caa0646f416a28dd1da4692428475079991c64c3
Sebastian Hiller <sebi-hiller@t-online.de>
2025-05-22 16:38:36 +0200

    hot fix

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     // Set this to empty string for local development
     //baseUrl: '',
     // Set this to 'https://christianetretter.github.io/STAMP' for production
-    baseUrl: 'https://christianetretter.github.io/STAMP'
+    baseUrl: 'https://christianetretter.github.io/STAMP/'
 };
 
 export default config; 
